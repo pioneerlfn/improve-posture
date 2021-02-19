ca91761979eb284a680defd332c724387e97bbd2
FrankLiu <liufuniu@didiglobal.com>
2021-02-20 01:13:26 +0800

    notes

--- go/slice.go
+++ go/slice.go
@@ -9,12 +9,12 @@ func main() {
 	var s1 []int
 	var s2 = []int{}
 
-	fmt.Println(s1 == nil)
-	fmt.Println(s2 == nil)
+	fmt.Println(s1 == nil) // true
+	fmt.Println(s2 == nil) // false
 
 	p1 := *(*[3]int)(unsafe.Pointer(&s1))
 	p2 := *(*[3]int)(unsafe.Pointer(&s2))
 
-	fmt.Println(p1)
-	fmt.Println(p2)
+	fmt.Println(p1) // [0 0 0]
+	fmt.Println(p2) // [824634150592 0 0]
 }
